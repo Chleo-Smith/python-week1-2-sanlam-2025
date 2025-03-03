cc91f375fe5526df52c73371f51e892f4c4fe0e9
Chleo-Smith <Chleo.Smith@sanlam.co.za>
2025-03-03 15:22:28 +0200

    adding auto gen documentation

--- Day_09/fun.py
+++ Day_09/fun.py
@@ -2,6 +2,14 @@ from cool import to_upper_case
 
 
 def powerful(n):
+    """_summary_
+
+    Args:
+        n (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return n * n
 
 
