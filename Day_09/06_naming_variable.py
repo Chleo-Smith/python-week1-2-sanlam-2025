84213dda62b7b6d9c805ff8a21b0bfafee8a3d69
Chleo-Smith <Chleo.Smith@sanlam.co.za>
2025-03-03 15:36:17 +0200

    naming variables

---
+++ Day_09/06_naming_variable.py
@@ -0,0 +1,12 @@
+# variable names
+# SYNTAX rules
+# 1. avoid keywords - if, else, elif, while, for
+# 2. dont override inbuilt functions
+# 3. cant start with number
+# 4. cant use special sign. except _
+
+
+# good practice
+# 1. snake case
+# 2. be descriptive(but keep it short)
+# 3. CONSTANT_CASE for constant variables
