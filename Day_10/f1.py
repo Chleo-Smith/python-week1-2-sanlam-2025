acbf26b39dead9aa8dff88e3c4baa097f39c3a6b
Chleo-Smith <Chleo.Smith@sanlam.co.za>
2025-03-04 09:04:46 +0200

    main = name templates

--- Day_10/f1.py
+++ Day_10/f1.py
@@ -2,5 +2,9 @@ def add(t1, t2):
     return t1 + t2
 
 
+def main():
+    print("Hello from f1")
+
+
 if __name__ == "__main__":
-    print("Hello, from f1")
+    main()
--- Day_10/f2.py
+++ Day_10/f2.py
@@ -1,4 +1,10 @@
 import f1
 
-print("Hello from f2")
-print("sum: ", f1.add(10, 20))
+
+def main():
+    print("Hello from f2")
+    print("sum: ", f1.add(10, 20))
+
+
+if __name__ == "__main__":
+    main()
